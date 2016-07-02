60bd8e136d953c9a9edf7e0b9f12c00599c2f5e2
Aaron Scruggs <aarondscruggs@gmail.com>
2016-07-01 17:41:40 -0700

    Add ProfileCheck endpoint

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 
-from profiles.views import EmployeeProfileListCreateView
+from profiles.views import EmployeeProfileListCreateView, ProfileCheck
 
 urlpatterns = [
     url(r'^employee/$', EmployeeProfileListCreateView.as_view(),
-        name="employee_list")
+        name="employee_list"),
+    url(r'^check/$', ProfileCheck.as_view(),
+        name="profile_check"),
 
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework import generics, status
 
-from profiles.models import EmployeeProfile
+from profiles.models import EmployeeProfile, ManagerProfile
 from profiles.serializers import EmployeeProfileSerializer
 
 """
@@ -14,3 +16,26 @@ Views for updating and creating profiles
 class EmployeeProfileListCreateView(generics.ListAPIView):
     queryset = EmployeeProfile.objects.all()
     serializer_class = EmployeeProfileSerializer
+
+
+class ProfileCheck(APIView):
+    """
+    Takes a GET request with a token and returns an object with the type of
+    profile the user has under 'type':
+    Possible responses: {"type": "employee"}, {"type": "manager"},
+     {"type": "no token"}, {"type": "no profile"}
+    """
+
+    def get(self, request, format=None):
+        if not request.user.id:
+            return Response({"type": "no token"})
+
+        if EmployeeProfile.objects.filter(user=request.user).first():
+            return Response({"type": "employee"})
+        elif ManagerProfile.objects.filter(user=request.user).first():
+            return Response({"type": "manager"})
+        else:
+            return Response({"type": "no profile"})
+
+
+
