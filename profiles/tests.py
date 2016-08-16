6f4db9dbb310a1f40153d1f70809d1ee4ad383b6
Aaron Scruggs <aarondscruggs@gmail.com>
2016-08-15 20:07:24 -0700

    Fix error message response

--- profiles/tests.py
+++ profiles/tests.py
@@ -238,4 +238,4 @@ class EmailTests(APITestCase):
         response = self.client.post(url, data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data, 'email error')
+        self.assertEqual(response.data, 'No employee profile found')
